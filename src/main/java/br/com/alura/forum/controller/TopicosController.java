84fb66bd148684bb84332dbefe2865349dedd65c
renantavaresti <62315055+renantavaresti@users.noreply.github.com>
2020-10-11 14:26:32 -0300

    Filtrando a consulta dos topicos utilizando o findBy da interface JpaRepository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,9 +1,11 @@
 package br.com.alura.forum.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.respository.TopicoRepository;
@@ -17,9 +19,16 @@ public class TopicosController {
 	//Temos que ter um método mapeando o endereço
 	//A lógica nesse trecho seria carregar a lista com todos os tópicos e devolver ela pra quem fez a chamada.
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista() {
-		List<Topico> topicos = topicoRepository.findAll();
-		return TopicoDto.converter(topicos);
-	}
+	public List<TopicoDto> lista(String nomeCurso) {
+		if(nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+				
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+		}
+		
 
 }
--- src/main/java/br/com/alura/forum/respository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/respository/TopicoRepository.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.respository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Topico;
@@ -8,5 +10,8 @@ import br.com.alura.forum.modelo.Topico;
 // essa interface eu precisa herdar ela de alguma interface... no caso JPARepository
 // JpaRespository tem o generics onde tenho q passar a entidade e qual tipo do atributo do ID dessa entidade
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCurso_Nome(String nomeCurso);
+
 
 }
