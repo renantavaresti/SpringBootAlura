d0b8ad5462698fdee64698b87dde55acfb80b21f
renantavaresti <62315055+renantavaresti@users.noreply.github.com>
2020-10-11 15:35:39 -0300

    Ajustando o cadastro de topicos em TopicosController para retornar um ResponseEntity

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
@@ -41,9 +44,12 @@ public class TopicosController {
 		}
 		
 	@PostMapping
-	public void cadastras(@RequestBody TopicoForm form) { //RequestBody Spring, esse parametro é para vc pegar no corpo da requisicao e nao na URL
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) { //RequestBody Spring, esse parametro é para vc pegar no corpo da requisicao e nao na URL
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 	
 
--- src/main/java/br/com/alura/forum/respository/CursoRepository.java
+++ src/main/java/br/com/alura/forum/respository/CursoRepository.java
@@ -1,7 +1,5 @@
 package br.com.alura.forum.respository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Curso;
