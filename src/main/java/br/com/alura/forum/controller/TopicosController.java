91e2c66449c542deb9798ecad80cb5ddab96c4df
renantavaresti <62315055+renantavaresti@users.noreply.github.com>
2020-10-11 13:05:31 -0300

    Utilizando o findALL() para trazer todas as litas de topicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,14 +1,10 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.respository.TopicoRepository;
 
@@ -22,8 +18,8 @@ public class TopicosController {
 	//A lógica nesse trecho seria carregar a lista com todos os tópicos e devolver ela pra quem fez a chamada.
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista() {
-		Topico topico = new Topico("Dúvida spring-boot:run", "Dúvida com Spring", new Curso("Spring", "Programação"));
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		List<Topico> topicos = topicoRepository.findAll();
+		return TopicoDto.converter(topicos);
 	}
 
 }
