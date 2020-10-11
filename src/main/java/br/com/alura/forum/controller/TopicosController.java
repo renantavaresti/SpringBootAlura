2d92f9e9374522d385a4b8b52bc61a1dc897cee1
renantavaresti <62315055+renantavaresti@users.noreply.github.com>
2020-10-11 13:02:30 -0300

    Injetando a dependencia na classe TopicosController para utilizacao da interface TopicoRepository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,15 +3,21 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.respository.TopicoRepository;
 
 @RestController
 public class TopicosController {
+	
+	@Autowired 	
+	private TopicoRepository topicoRepository;
+	
 	//Temos que ter um método mapeando o endereço
 	//A lógica nesse trecho seria carregar a lista com todos os tópicos e devolver ela pra quem fez a chamada.
 	@RequestMapping("/topicos")
